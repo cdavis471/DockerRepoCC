8a837cc457926944fdd7cd2b833e71d98f40d023
Ubuntu <c20441826@CCVM.nv1u1j5pb1yelbpkqfssbglhoc.ax.internal.cloudapp.net>
2022-10-17 23:50:00 +0000

    Lab 2 Commit

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -4,7 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
-                    <TodoListCard />
+                    <TodoListCard/>
                 </Col>
             </Row>
         </Container>
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one above!</p>
+                <p className="text-center">Enter your favourite countries that you would like to visit!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="Enter Country..."
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add Country'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
@@ -166,7 +166,7 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
                         size="sm"
                         variant="link"
                         onClick={removeItem}
-                        aria-label="Remove Item"
+                        aria-label="Remove Country"
                     >
                         <i className="fa fa-trash text-danger" />
                     </Button>
